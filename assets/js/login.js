62c37288a32d4ce93197f7e0d06ad2c7258fa7c9
sky_neblua <3167298120@qq.com>
2023-06-13 21:49:07 +0800

    登录注册页面logi.htmln完成了

--- assets/js/login.js
+++ assets/js/login.js
@@ -33,5 +33,34 @@ $(function() {
   })
 
   // 监听注册表单的提交事件
-  
+  $('#form_reg').on('submit', function(e) {
+    e.preventDefault()
+    let data = {
+      username: $('#form_reg input[name="username"]').val(),
+      password: $('#form_reg input[name="password"]')[1].value
+    }
+    $.post('/api/reguser', data, function(res) {
+      if(res.status !== 0) return alert(res.message)
+      console.log(layer)
+      layer.msg('注册成功，请登录！')
+      $('#link_login').click()
+    })
+  })
+})
+
+// 监听登录表单的提交事件
+$('#form_login').on('submit', function(e) {
+  e.preventDefault()
+  $.ajax({
+    url: '/api/login',
+    method: 'POST',
+    data: $(this).serialize(),
+    success: function(res) {
+      console.log(res.status)
+      if(res.status !== 0) return layer.msg('登录失败！')
+      layer.msg('登录成功')
+      localStorage.setItem('token', res.token)
+      location.href = '/index.html'
+    }
+  })
 })
